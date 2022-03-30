e9978bfa9266f8af41ed0299e583accd79545bfd
Christoph Brosch <christoph.brosch@outlook.de>
2022-03-30 16:09:58 +0200

    added environmental variables to vercel

--- core/firebase.ts
+++ core/firebase.ts
@@ -5,12 +5,12 @@ import { getDatabase, ref, child, get } from "firebase/database";
 
 // TODO: Add SDKs for Firebase products that you want to use
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-    appId: process.env.REACT_APP_FIREBASE_APPID
+    apiKey: process.env.FIREBASE_APIKEY,
+    authDomain: process.env.FIREBASE_AUTHDOMAIN,
+    projectId: process.env.FIREBASE_PROJECTID,
+    storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.FIREBASE_APPID
 };
 
 const app = initializeApp(firebaseConfig);
